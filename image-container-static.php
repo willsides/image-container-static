e1688adddff07cb3ebd277dfb27df9f81a188af7
Will Sides <32076464+willsides@users.noreply.github.com>
2023-11-10 22:59:20 -0800

    Update versioning

--- image-container-static.php
+++ image-container-static.php
@@ -5,7 +5,7 @@
  * Description:       Displays an image as a background over nested blocks.
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           0.1.0
+ * Version:           1.0.0
  * Author:            Will Sides
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
